e820e8023ec93818770b43644ac5a30d2b113e41
nepalrohan <rohannepal098@gmail.com>
2024-06-20 20:51:45 -0700

    Initialize backend and database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 
 
-app.listen(3000, ()=>{
-    console.log("Server is running at port 3000")
+mongoose.connect(process.env.MONGO).then(
+    ()=>{
+        console.log("Database is connected !")
+        app.listen(3000, ()=>{
+            console.log("Server is running at port 3000")
+        })
+    }
+).catch((err)=>{
+    console.log(err)
 })
+
