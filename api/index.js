93b812156b2c6a73ac69ef3fa779ecd233a1527d
nepalrohan <rohannepal098@gmail.com>
2024-06-21 22:34:45 -0700

    Add funcationality to signup page

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
 const app = express();
 import userRoutes from './routes/user.route.js'
 import authRoute from './routes/authroute.js'
 app.use(express.json());
+app.use(cors());
 mongoose.connect(process.env.MONGO).then(
     ()=>{
         console.log("Database is connected !")
-        app.listen(5000, ()=>{
-            console.log("Server is running at port 5000")
+        app.listen(3000, ()=>{
+            console.log("Server is running at port 3000")
         })
     }
 ).catch((err)=>{
