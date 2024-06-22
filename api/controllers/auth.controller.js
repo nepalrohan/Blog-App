20fb3eda497b632612c522ecfa84fd3786d1e64c
nepalrohan <rohannepal098@gmail.com>
2024-06-22 04:22:59 -0700

    Add functionality to signin page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,65 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-export const signup = async(req, res, next)=>{
-const {username, email, password} = req.body;
+  if (
+    !username ||
+    !password ||
+    !email ||
+    username === "" ||
+    password === "" ||
+    email === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+  }
 
-if(!username || !password || !email || username==='' || password==='' || email===''){
-next(errorHandler(400, 'All fields are required'))
-}
+  const hashpassword = bcryptjs.hashSync(password, 10);
 
-const hashpassword = bcryptjs.hashSync(password, 10);
-
-const newUser = new User({
+  const newUser = new User({
     email,
-    password:hashpassword,
-    username
-});
+    password: hashpassword,
+    username,
+  });
 
-try{
+  try {
     await newUser.save();
-res.json({message:"Signup sucesssful"})
-}
-catch (error){
-next(error);
-}
+    res.json({ message: "Signup sucesssful" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) {
+     return  next(errorHandler(404, "Invalid Credientials"));
+    }
+
+    const validpassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validpassword) {
+     return next(errorHandler(400, "Invalid Credientials"));
+    }
+    const token = jwt.sign(
+      {
+        id: validuser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const {password:pass, ...rest} = validuser._doc;
+    res.status(200).cookie('access_token', token,{
+        httpOnly:true
+    }).json(rest)
+  } catch (error) {
+    next(error);
+  }
+};
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,118 @@
-
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react';
 const SignIn = () => {
+
+  const [formData, setFormData] =useState({
+    email:'',
+    password:'',
+  
+  });
+
+
+  const navigate = useNavigate();
+  const [errormessage, seterrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e)=>{
+setFormData({...formData, [e.target.id]: e.target.value.trim()});
+  }
+
+
+  const handleSubmit = async (e)=>{
+e.preventDefault();
+if(!formData.email || !formData.password){
+  return seterrorMessage('Please fill all fields');
+}
+try {
+  setLoading(true);
+  seterrorMessage(null);
+  const res = await fetch('/api/auth/signin',{
+    method:'POST',
+    headers:{'Content-Type': 'application/json'},
+    body:JSON.stringify(formData)
+  })
+  const data = await res.json();
+if(data.sucess === false){
+  return seterrorMessage(data.message)
+
+}
+
+// loading
+setLoading(false)
+if(res.ok){
+  navigate('/');
+}
+} 
+catch (error) {
+  seterrorMessage(error.message)
+  setLoading(false)
+}
+  }
+
   return (
-    <div>SignIn</div>
+    <div className="min-h-screen mt-20">
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <div className='flex-1'>
+{/* for left side */}
+<Link to='/' className=' sm:text-xl font-bold dark:text-white text-4xl'>
+    <span className="px-2 text-white py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">Blog</span>
+    .com
+</Link>
+<p className='text-sm mt-5'>
+  Blog.com, a modern web service to publish your blogs and articles worldwide. Sign up now!
+</p>
+        </div>
+
+        <div className='flex-1'>
+{/* for right side */}
+<form className='felx felx-col gap-4 '  onSubmit={handleSubmit}>
+
+  <div>
+    <Label value='Email' />
+    <TextInput
+    type='email'
+    placeholder='Enter email'
+    id='email'
+    onChange={handleChange}
+     />
+  </div>
+  <div>
+    <Label value='Password' />
+    <TextInput
+    type='password'
+    placeholder='Enter password'
+    id='password'
+    onChange={handleChange}
+     />
+  </div>
+  <Button className='w-full mt-4' gradientDuoTone='purpleToPink'
+   type='submit'
+   disabled={loading}
+   >
+   {
+loading ? (
+<>
+<Spinner size='sm' />
+<span className='pl-3'>Loading....</span>
+</>
+): 'Sign In'
+   }
+   </Button>
+
+</form>
+<div className='flex gap-2 text-sm mt-5'>
+  <span>Dont have an account?</span>
+  <Link to='/sign-up' className='text-blue-500'>Sign Up</Link>
+</div>
+{errormessage && (
+<Alert className='mt-5' color='failure'>
+{errormessage}
+</Alert>
+)}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
